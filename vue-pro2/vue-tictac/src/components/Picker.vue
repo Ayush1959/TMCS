1e492b5f22017467fe028efc93484617d3b2e16b
Ayush <srihari.ayush@gmail.com>
2018-11-21 11:24:37 +0530

    code as on 21/11/18

---
+++ vue-pro2/vue-tictac/src/components/Picker.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="container">
+        <div v-html="dataSel"></div>
+        <button @click="boldtxt"> B </button>
+        <button @click="italictxt"> I </button>
+        <button @click="striketxt"> S </button>
+        <!-- <button> U </button> -->
+        <form action="">
+            <textarea @input="submit" name="mytextarea" id="mytextarea" cols="120" rows="20"></textarea>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dataSel: null,
+      blt: 1,
+      ilt: 1,
+      slt: 1
+    };
+  },
+  methods: {
+    submit() {
+      this.dataSel = document.getElementById("mytextarea").value;
+    },
+    boldtxt() {
+      var txtarea = document.getElementById("mytextarea");
+      var txtar = document.getElementById("mytextarea").value;
+      var start = txtarea.selectionStart;
+      var finish = txtarea.selectionEnd;
+      var sel = txtarea.value.substring(start, finish);
+      if (sel.length > 0) {
+        var rep = txtarea.value.substring(start, finish).bold();
+        var xx = document.getElementById("mytextarea").value.replace(sel, rep);
+      } else {
+        if (this.blt > 0) {
+          var rep = "<b>";
+          var xx = document.getElementById("mytextarea").value.concat(rep);
+          this.blt = 0;
+        } else {
+          var rep = "</b>";
+          var xx = document.getElementById("mytextarea").value.concat(rep);
+          this.blt = 1;
+        }
+      }
+      this.dataSel = xx;
+      document.getElementById("mytextarea").value = this.dataSel;
+    },
+    italictxt() {
+      var txtarea = document.getElementById("mytextarea");
+      var txtar = document.getElementById("mytextarea").value;
+      var start = txtarea.selectionStart;
+      var finish = txtarea.selectionEnd;
+      var sel = txtarea.value.substring(start, finish);
+      if (sel.length > 0) {
+        var rep = txtarea.value.substring(start, finish).italics();
+        var xx = document.getElementById("mytextarea").value.replace(sel, rep);
+      } else {
+        if (this.ilt > 0) {
+          var rep = "<i>";
+          var xx = document.getElementById("mytextarea").value.concat(rep);
+          this.ilt = 0;
+        } else {
+          var rep = "</i>";
+          var xx = document.getElementById("mytextarea").value.concat(rep);
+          this.ilt = 1;
+        }
+      }
+      this.dataSel = xx;
+      document.getElementById("mytextarea").value = this.dataSel;
+    },
+    striketxt() {
+      var txtarea = document.getElementById("mytextarea");
+      var txtar = document.getElementById("mytextarea").value;
+      var start = txtarea.selectionStart;
+      var finish = txtarea.selectionEnd;
+      var sel = txtarea.value.substring(start, finish);
+      if (sel.length > 0) {
+        var rep = txtarea.value.substring(start, finish).strike();
+        var xx = document.getElementById("mytextarea").value.replace(sel, rep);
+      } else {
+        if (this.slt > 0) {
+          var rep = "<strike>";
+          var xx = document.getElementById("mytextarea").value.concat(rep);
+          this.slt = 0;
+        } else {
+          var rep = "</strike>";
+          var xx = document.getElementById("mytextarea").value.concat(rep);
+          this.slt = 1;
+        }
+      }
+      this.dataSel = xx;
+      document.getElementById("mytextarea").value = this.dataSel;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-pro2/vue-tictac/src/components/Wizard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div>
+    <Picker/>
+    <editor-content :editor="editor" />  
+    </div>   
+    <!-- Actions -->
+      <div class="buttons">        
+        <button
+          @click="goNext"
+          class="btn"
+        >submit</button>
+      </div>
+      <div class="loading-wrapper" v-if="nxt === 'pending'">
+      <div class="loader">
+        <!-- <img src="../assets/spinner.svg" alt="spinner"> -->
+        <p>Please Wait we are hitting our servers</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Picker from "./Picker";
+import Editor from "../../node_modules/tiptap/src/Editor";
+import EditorContent from "../../node_modules/tiptap/src/Components/EditorContent";
+
+export default {
+  name: "Wizard",
+  components: {
+    Picker,
+    EditorContent
+  },
+  data() {
+    return {
+      editor: null
+    };
+  },
+  methods: {
+    goNext() {
+      var x = this;
+    }
+  },
+  mounted() {
+    this.editor = new Editor({
+      content: "<p>This is just a boring paragraph</p>"
+    });
+  },
+  beforeDestroy() {
+    this.editor.destroy();
+  }
+};
+</script>
+
+<style>
+</style>
